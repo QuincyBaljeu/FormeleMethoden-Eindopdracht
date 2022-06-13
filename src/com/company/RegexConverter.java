0014b430a29f27e10134d6992bb6a2194d35562f
Quincy Baljeu <q.baljeu@gmail,com>
2022-06-13 13:21:38 +0200

    Ik weet het niet meer en het boeit met ook niet

--- src/com/company/RegexConverter.java
+++ src/com/company/RegexConverter.java
@@ -25,7 +25,7 @@ public class RegexConverter {
             startsWith = regex.substring(regex.indexOf("^") + 1, regex.indexOf("/"));
         } else if (regex.contains("^") && regex.contains("$")) {
             startsWith = regex.substring(regex.indexOf("^") + 1, regex.indexOf("$"));
-        } else {
+        } else if(regex.contains("^")){
             startsWith = regex.substring(regex.indexOf("^") + 1);
         }
 
@@ -73,6 +73,7 @@ public class RegexConverter {
 
         public void addContainsNodes(NDFA ndfa, String containsString){
             NDFANode initialStartNode = new NDFANode(containsString.charAt(0));
+            initialStartNode.addTransition(initialStartNode);
             ndfa.getAutomata().add(initialStartNode);
 
             //starts at one to compensate for manual adding of first node
@@ -80,17 +81,17 @@ public class RegexConverter {
                 NDFANode newNode = new NDFANode(containsString.charAt(i));
                 //System.out.println(newNode.toString());
                 ndfa.getAutomata().get(i - 1).addTransition(newNode);
-                ndfa.getAutomata().add(newNode);
-
                 if(i == containsString.length() - 1) {
-                    ndfa.getAutomata().get(i-1).addTransition(ndfa.getAutomata().get(i-1));
+                    newNode.addTransition(newNode);
                     ndfa.resetStopNode();
                 }
+                ndfa.getAutomata().add(newNode);
             }
         }
 
         public void addEndNode(NDFA ndfa, String endString){
             NDFANode initialStartNode = new NDFANode(endString.charAt(0));
+            initialStartNode.addTransition(initialStartNode);
             ndfa.getAutomata().add(initialStartNode);
 
             //starts at one to compensate for manual adding of first node
@@ -98,14 +99,11 @@ public class RegexConverter {
                 NDFANode newNode = new NDFANode(endString.charAt(i));
                 //System.out.println(newNode.toString());
                 ndfa.getAutomata().get(i - 1).addTransition(newNode);
-                ndfa.getAutomata().add(newNode);
-
                 if(i == endString.length() - 1) {
-
                     ndfa.resetStopNode();
                 }
+                ndfa.getAutomata().add(newNode);
             }
-
         }
     }
 
