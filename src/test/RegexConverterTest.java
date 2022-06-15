d6eede6f7f01f405d797e0934f94065b02c1035f
Quincy Baljeu <q.baljeu@gmail,com>
2022-06-15 15:18:48 +0200

    Progress to tests

--- src/test/RegexConverterTest.java
+++ src/test/RegexConverterTest.java
@@ -11,38 +11,65 @@ import static org.junit.jupiter.api.Assertions.*;
 class RegexConverterTest {
 
     @Test
-    void addStartNodes() {
+    void addStartNodesAmount() {
         NDFA testNdfa = new NDFA();
-        NDFA correctNdfa = new NDFA();
 
         RegexConverter converter = new RegexConverter();
         converter.addStartNodes(testNdfa, "abbc");
 
-        Node a = new Node('a');
-        Node b = new Node('b');
-        Node b2 = new Node('b');
-        Node c = new Node('c');
-
-        a.addTransition(b);
-        b.addTransition(b2);
-        b2.addTransition(c);
-        c.addTransition(c);
-
-        c.setStopNode(true);
-
-        correctNdfa.addNode(a);
-        correctNdfa.addNode(b);
-        correctNdfa.addNode(b2);
-        correctNdfa.addNode(c);
-
-        assertArrayEquals(correctNdfa.getAutomata().toArray(), testNdfa.getAutomata().toArray());
+        assertEquals(testNdfa.getAutomata().size(), 4);
     }
 
     @Test
-    void addContainsNodes() {
+    void addContainsNodesAmount() {
+        NDFA testNdfa = new NDFA();
+
+        RegexConverter converter = new RegexConverter();
+        converter.addContainsNodes(testNdfa, "abbc");
+
+        assertEquals(testNdfa.getAutomata().size(), 4);
     }
 
     @Test
-    void addEndNode() {
+    void addEndNodeAmount() {
+        NDFA testNdfa = new NDFA();
+
+        RegexConverter converter = new RegexConverter();
+        converter.addEndNode(testNdfa, "abbc");
+
+        assertEquals(testNdfa.getAutomata().size(), 4);
     }
+
+    @Test
+    void checkFinalNode(){
+        NDFA testNdfa = new NDFA();
+
+        RegexConverter converter = new RegexConverter();
+        converter.addStartNodes(testNdfa, "abb");
+        converter.addEndNode(testNdfa, "ccd");
+
+        //Get last node and check if it is true
+        assertTrue(testNdfa.getAutomata().get(testNdfa.getAutomata().size()-1).isStopNode());
+    }
+
+    @Test
+    void correctDFAConversion(){
+
+        NDFA testNdfa = new NDFA();
+        RegexConverter converter = new RegexConverter();
+
+        converter.addStartNodes(testNdfa, "ab");
+        converter.addEndNode(testNdfa, "cd");
+
+        testNdfa.convertToDFA("abcde");
+
+        boolean containsENode = false;
+
+
+
+    }
+    //Test if DFA conversion adds correct node
+    //Test if start/begin/end have correct loop nodes
+    //test if start/begin/edn add correct nodes
+
 }
